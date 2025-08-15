dc46a22cc3f29b090a12507726907b8f114cb77f
Atemukesu <120582291+atemukesu@users.noreply.github.com>
2025-08-15 22:15:24 +0800

    Delete

--- src/client/java/com/atemukesu/extendednoteblock/config/ModConfig.java
+++
@@ -1,8 +0,0 @@
-package com.atemukesu.extendednoteblock.config;
-
-public class ModConfig {
-    public String ffmpegPath = "";
-    public String fluidSynthPath = "";
-    public String activeSoundPackId = "";
-    public Integer threads;
-}
